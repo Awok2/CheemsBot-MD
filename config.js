0a1233c999584d513ab6a9d7019d719f3944349a
Azwanmd <100202744+Awok2@users.noreply.github.com>
2022-04-09 16:21:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,11 +22,11 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['916909137213','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['916909137213'] //another owner number
-global.premium = ['916909137213'] //premium number
+global.owner = ['6288291659138','6288210387012'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['6288210387012'] //another owner number
+global.premium = ['6285794615047'] //premium number
 global.pengguna = 'Xeon' //username
-global.botnma = 'Cheems Bot-MD' //bot name
+global.botnma = 'AZWAN OFX' //bot name
 global.ownernma = '🦄Dream Guy Xeon' //owner name
 global.packname = 'Cheems Bot' //sticker package name
 global.author = 'By Dream Guy Xeon' //sticker author name
